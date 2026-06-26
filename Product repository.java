c5fae35ce7b0320817190cc2a2d87aad02b46128
thennarasuselvaraj0608-coder <thennarasuselvaraj0608@gmail.com>
2026-06-26 21:04:14 +0530

    Delete Product repository.java

--- Product repository.java
+++
@@ -1,7 +0,0 @@
-package com.inventory.repository;
-
-import com.inventory.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
